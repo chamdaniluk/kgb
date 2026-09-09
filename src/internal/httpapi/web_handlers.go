0eeda84edfd8fc588e90605c14845ca86d2ad5d6
chamdaniluk <chamdani.chahalim@gmail.com>
2026-09-09 14:00:53 +0700

    chore: assetVersion 20260909c utk form + penanda merah baru

--- src/internal/httpapi/web_handlers.go
+++ src/internal/httpapi/web_handlers.go
@@ -16,7 +16,7 @@ var logoGroboganPNG []byte
 //go:embed assets/fonts/plus-jakarta-sans-var.woff2
 var fontPJSVar []byte
 
-const assetVersion = "20260909b"
+const assetVersion = "20260909c"
 
 type pageData struct {
 	Title   string
