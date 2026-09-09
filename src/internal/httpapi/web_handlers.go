5913ce6d7aa9b876394e5b49f4e9c6b563b6cbf9
chamdaniluk <chamdani.chahalim@gmail.com>
2026-09-09 14:29:34 +0700

    chore: assetVersion 20260909d utk tab Pantauan Unit

--- src/internal/httpapi/web_handlers.go
+++ src/internal/httpapi/web_handlers.go
@@ -16,7 +16,7 @@ var logoGroboganPNG []byte
 //go:embed assets/fonts/plus-jakarta-sans-var.woff2
 var fontPJSVar []byte
 
-const assetVersion = "20260909c"
+const assetVersion = "20260909d"
 
 type pageData struct {
 	Title   string
