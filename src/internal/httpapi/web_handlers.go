ae392c5fa3b8b56cb8131e931c268a75c49614e1
chamdaniluk <chamdani.chahalim@gmail.com>
2026-09-10 20:31:14 +0700

    chore: assetVersion 20260909j utk MKG cetak mentah

--- src/internal/httpapi/web_handlers.go
+++ src/internal/httpapi/web_handlers.go
@@ -16,7 +16,7 @@ var logoGroboganPNG []byte
 //go:embed assets/fonts/plus-jakarta-sans-var.woff2
 var fontPJSVar []byte
 
-const assetVersion = "20260909i"
+const assetVersion = "20260909j"
 
 type pageData struct {
 	Title   string
