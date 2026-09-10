dafb935cc9e82346479cb03bdf1c153fe10599d6
chamdaniluk <chamdani.chahalim@gmail.com>
2026-09-10 12:12:14 +0700

    chore: assetVersion 20260909g utk preview MKG pemenang

--- src/internal/httpapi/web_handlers.go
+++ src/internal/httpapi/web_handlers.go
@@ -16,7 +16,7 @@ var logoGroboganPNG []byte
 //go:embed assets/fonts/plus-jakarta-sans-var.woff2
 var fontPJSVar []byte
 
-const assetVersion = "20260909f"
+const assetVersion = "20260909g"
 
 type pageData struct {
 	Title   string
