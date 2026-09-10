20e0474f3c3c43114740baa7dbf7613e4d20f6e6
chamdaniluk <chamdani.chahalim@gmail.com>
2026-09-10 12:41:17 +0700

    chore: assetVersion 20260909h utk TTE tolak + pratinjau PDF

--- src/internal/httpapi/web_handlers.go
+++ src/internal/httpapi/web_handlers.go
@@ -16,7 +16,7 @@ var logoGroboganPNG []byte
 //go:embed assets/fonts/plus-jakarta-sans-var.woff2
 var fontPJSVar []byte
 
-const assetVersion = "20260909g"
+const assetVersion = "20260909h"
 
 type pageData struct {
 	Title   string
